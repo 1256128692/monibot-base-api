d6e8f97ebe3f4d15171584c8afdd271c1cf6af7e
gaoxu <gaoxao004@gmail.com>
2023-02-21 17:19:05 +0800

    文档修改

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/ProjectController.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/ProjectController.java
@@ -1,5 +1,7 @@
 package cn.shmedo.monitor.monibotbaseapi.controller;
 
+import cn.shmedo.iot.entity.annotations.Permission;
+import cn.shmedo.iot.entity.base.CommonVariable;
 import cn.shmedo.monitor.monibotbaseapi.config.DefaultConstant;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +17,7 @@ public class ProjectController {
     /**
      * @api {POST} /AddProject 新增项目
      * @apiVersion 1.0.0
-     * @apiGroup 项目模块
+     * @apiGroup 工程项目管理模块
      * @apiName AddProject
      * @apiDescription 新增项目
      * @apiParam (请求体) {Int} companyID 公司ID
@@ -59,11 +61,138 @@ public class ProjectController {
     public Object addProject() {
         return null;
     }
+    /**
+     * @api {post} /QueryProjectPageList 分页查询工程项目列表
+     * @apiDescription 分页查询工程项目列表
+     * @apiVersion 1.0.0
+     * @apiGroup 工程项目管理
+     * @apiName QueryProjectPageList
+     * @apiParam {String} [projectName] 项目名称,支持模糊查询
+     * @apiParam {String} [directlyManageUnit] 直管单位,支持模糊查询
+     * @apiParam {String} [companyName] 企业名称,支持模糊查询
+     * @apiParam {Int[]} [projectTypeList] 项目类型列表
+     * @apiParam {Int} [status] 项目状态，null:全选，1:启用，0:停用
+     * @apiParam {Int[]} [platformTypeList] 平台类型列表
+     * @apiParam {DateTime} [verifyDate] 有效期
+     * @apiParam {DateTime} [beginCreateTime] 创建时间-开始
+     * @apiParam {DateTime} [endCreatTime] 创建时间-结束
+     * @apiParam {Object[]} [propertyQueryEntity] 属性查询实体
+     * @apiParam {Int} propertyQueryEntity.propertyID 属性ID
+     * @apiParam {String} [propertyQueryEntity.value] 属性值，仅字符串类型支持模糊查询
+     * @apiParam {Int} pageSize 页大小
+     * @apiParam {Int} currentPage 当前页
+     * @apiSuccess (返回结果) {Int} totalCount 数据总量
+     * @apiSuccess (返回结果) {Int} totalPage 总页数
+     * @apiSuccess (返回结果) {Object[]} dataList 项目信息列表
+     * @apiSuccess (返回结果) {Int} dataList.projectID 项目id
+     * @apiSuccess (返回结果) {Int} dataList.companyID 公司id
+     * @apiSuccess (返回结果) {String} dataList.companyName 所属公司名称
+     * @apiSuccess (返回结果) {String} dataList.projectName 项目名称
+     * @apiSuccess (返回结果) {String} dataList.shortName 项目简称
+     * @apiSuccess (返回结果) {Int} dataList.projectType 项目类型
+     * @apiSuccess (返回结果) {Int} dataList.projectTypeName 项目类型名称
+     * @apiSuccess (返回结果) {Int} dataList.PlatformType 平台类型
+     * @apiSuccess (返回结果) {Int} dataList.PlatformTypeName 平台名称
+     * @apiSuccess (返回结果) {String} dataList.directlyManageUnit 直管单位
+     * @apiSuccess (返回结果) {DateTime} dataList.ExpiryDate 项目有效期
+     * @apiSuccess (返回结果) {Bool} dataList.isValid 是否有效
+     * @apiSuccess (返回结果) {String} dataList.projectAddress 项目地址
+     * @apiSuccess (返回结果) {Double} dataList.latitude 项目经度
+     * @apiSuccess (返回结果) {Double} dataList.longitude 项目纬度
+     * @apiSuccess (返回结果) {String} [dataList.imagePath] 项目图片地址
+     * @apiSuccess (返回结果) {String} [dataList.desc] 项目简介
+     * @apiSuccess (返回结果) {DateTime} dataList.createTime 创建时间
+     * @apiSuccess (返回结果) {Int} dataList.createUserID 创建用户ID
+     * @apiSuccess (返回结果) {DateTime} dataList.updateTime 修改时间
+     * @apiSuccess (返回结果) {Int} dataList.updateUserID 修改用户ID
+     * @apiSampleRequest off
+     * @apiPermission 项目权限
+     */
+    //查询-添加条件还没有设计
+    @RequestMapping(value = "/QueryProjectPageList", method = RequestMethod.POST, produces = CommonVariable.JSON)
+    public Object queryProjectList(String name){
+        return null;
+    }
+
+
+    /**
+     * @api {post} /QueryProjectInfo 查询工程项目详情
+     * @apiDescription 查询工程项目详情
+     * @apiVersion 1.0.0
+     * @apiGroup 工程项目管理
+     * @apiName QueryProjectInfo
+     * @apiParam {Int} projectID 项目ID
+     * @apiSuccess (返回结果) {Int} projectID 项目id
+     * @apiSuccess (返回结果) {Int} companyID 公司id
+     * @apiSuccess (返回结果) {String} companyName 所属公司名称
+     * @apiSuccess (返回结果) {String} projectName 项目名称
+     * @apiSuccess (返回结果) {String} shortName 项目简称
+     * @apiSuccess (返回结果) {Int} projectType 项目类型
+     * @apiSuccess (返回结果) {Int} projectTypeName 项目类型名称
+     * @apiSuccess (返回结果) {Int} platformType 平台类型
+     * @apiSuccess (返回结果) {Int} platformTypeName 平台名称
+     * @apiSuccess (返回结果) {String} directlyManageUnit 直管单位
+     * @apiSuccess (返回结果) {DateTime} expiryDate 项目有效期
+     * @apiSuccess (返回结果) {Bool} isValid 是否有效
+     * @apiSuccess (返回结果) {String} projectAddress 项目地址
+     * @apiSuccess (返回结果) {Double} latitude 项目经度
+     * @apiSuccess (返回结果) {Double} longitude 项目纬度
+     * @apiSuccess (返回结果) {String} [imagePath] 项目图片地址
+     * @apiSuccess (返回结果) {String} [desc] 项目简介
+     * @apiSuccess (返回结果) {DateTime} createTime 创建时间
+     * @apiSuccess (返回结果) {Int} createUserID 创建用户ID
+     * @apiSuccess (返回结果) {DateTime} updateTime 修改时间
+     * @apiSuccess (返回结果) {Int} updateUserID 修改用户ID
+     * @apiSuccess (返回结果) {Object[]} propertyList 项目基础信息列表
+     * @apiSuccess (返回结果) {Int} propertyList.propertyID 项目属性ID
+     * @apiSuccess (返回结果) {Int} propertyList.propertyType 属性类型:1.数值,2.字符串,3.枚举,4.日期时间
+     * @apiSuccess (返回结果) {String} propertyList.className 结构名称
+     * @apiSuccess (返回结果) {String} propertyList.name 属性名称
+     * @apiSuccess (返回结果) {Bool} propertyList.required 是否必填
+     * @apiSuccess (返回结果) {Bool} propertyList.multiSelect 是否多选
+     * @apiSuccess (返回结果) {Int} propertyList.createType 创建类型 0-预定义 1-自定义
+     * @apiSuccess (返回结果) {String} [propertyList.enumField] 枚举字段
+     * @apiSuccess (返回结果) {String} [propertyList.unit] 属性单位
+     * @apiSuccess (返回结果) {String} [propertyList.value] 属性值
+     * @apiSampleRequest off
+     * @apiPermission 项目权限
+     */
+    @RequestMapping(value = "/QueryProjectInfo", method = RequestMethod.POST, produces = CommonVariable.JSON)
+    @Permission
+    public Object queryProjectInfo(){
+        return null;
+    }
+
+    /**
+     * @api {post} /UpdateProject 修改工程项目
+     * @apiDescription 修改工程项目
+     * @apiVersion 1.0.0
+     * @apiGroup 工程项目管理
+     * @apiName UpdateProject
+     * @apiParam {Int} projectID 项目ID
+     * @apiParam {String} projectName 项目名称
+     * @apiParam {String} shortName 项目简称
+     * @apiParam {String} [directlyManageUnit] 直管单位
+     * @apiParam {DateTime} expiryDate 有效期
+     * @apiParam {Int} status 项目状态,1启用，0停用
+     * @apiParam {String} projectAddress 项目地址
+     * @apiParam {Object[]} tagData 标签信息
+     * @apiParam {String} tagKey 标签键
+     * @apiParam {String} tagValue 标签值
+     * @apiSuccess (返回结果) {String} none 空
+     * @apiSampleRequest off
+     * @apiPermission 项目权限
+     */
+    @RequestMapping(value = "/UpdateProjectData", method = RequestMethod.POST, produces = CommonVariable.JSON)
+    @Permission
+    public Object updateProjectData(Object a){
+        return null;
+    }
 
     /**
      * @api {POST} /TransferProject 转移项目
      * @apiVersion 1.0.0
-     * @apiGroup 项目模块
+     * @apiGroup 工程项目管理模块
      * @apiName AddProject
      * @apiDescription 项目转移到其他企业
      * @apiParam (请求体) {Int} companyID 公司ID
@@ -80,7 +209,7 @@ public class ProjectController {
     /**
      * @api {POST} /RaiseExpiryDate 推迟有效期
      * @apiVersion 1.0.0
-     * @apiGroup 项目模块
+     * @apiGroup 工程项目管理模块
      * @apiName RaiseExpiryDate
      * @apiDescription 推迟有效期
      * @apiParam (请求体) {Int} projectID 项目ID
@@ -97,7 +226,7 @@ public class ProjectController {
     /**
      * @api {POST} /QueryModelList 查询模板
      * @apiVersion 1.0.0
-     * @apiGroup 项目模块
+     * @apiGroup 工程项目管理模块
      * @apiName QueryModelList
      * @apiDescription 查询模板列表
      * @apiParam (请求体) {Int} projectType 项目类型
@@ -122,6 +251,25 @@ public class ProjectController {
      */
     @RequestMapping(value = "QueryModelList", method = RequestMethod.POST, produces = DefaultConstant.JSON, consumes = DefaultConstant.JSON)
     public Object queryModelList() {
+        return null;
+    }
+
+    /**
+     * @api {POST} /UpdateProperty 设置属性
+     * @apiVersion 1.0.0
+     * @apiGroup 工程项目管理模块
+     * @apiName UpdateProperty
+     * @apiDescription 设置属性
+     * @apiParam (请求体) {Int} projectID 项目ID
+     * @apiParam (请求体) {Json[]} propertyList 属性列表
+     * @apiParam (请求体) {Int} propertyList.propertyID 属性ID
+     * @apiParam (请求体) {String} propertyList.value 属性值
+     * @apiSuccess (响应结果) {String} none  无
+     * @apiSampleRequest off
+     * @apiPermission xx权限:
+     */
+    @RequestMapping(value = "UpdateProperty", method = RequestMethod.POST, produces = DefaultConstant.JSON, consumes = DefaultConstant.JSON)
+    public Object updateProperty() {
         return null;
     }
 }
