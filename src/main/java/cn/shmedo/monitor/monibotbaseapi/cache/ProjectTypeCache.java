d365a1da4df40b8d2ede28c4e139883e5575832f
gaoxu <gaoxao004@gmail.com>
2023-02-24 13:24:00 +0800

    更新sql连接配置（关闭远程配置）

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/cache/ProjectTypeCache.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/cache/ProjectTypeCache.java
@@ -1,10 +1,10 @@
 package cn.shmedo.monitor.monibotbaseapi.cache;
 
-import cn.shmedo.monitor.monibotbaseapi.config.ContextHolder;
 import cn.shmedo.monitor.monibotbaseapi.dal.mapper.TbProjectTypeMapper;
 import cn.shmedo.monitor.monibotbaseapi.model.db.TbProjectType;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -15,16 +15,20 @@ import java.util.stream.Collectors;
  * @author: gaoxu
  * @create: 2023-02-23 17:14
  **/
-// @Component
+@Component
 public class ProjectTypeCache {
+
     private static List<TbProjectType> projectTypeList;
     public static Map<Integer, TbProjectType> projectTypeMap;
-    public ProjectTypeCache() {
-       // init();
-    }
 
+
+    private TbProjectTypeMapper tbProjectTypeMapper;
+    public ProjectTypeCache(TbProjectTypeMapper tbProjectTypeMapper) {
+        this.tbProjectTypeMapper = tbProjectTypeMapper;
+    }
+    @PostConstruct
     void init(){
-        projectTypeList = ContextHolder.getBean(TbProjectTypeMapper.class).selectAll();
+        projectTypeList = tbProjectTypeMapper.selectAll();
         projectTypeMap = projectTypeList.stream().collect(Collectors.toMap(TbProjectType::getID, Function.identity()));
     }
 }
