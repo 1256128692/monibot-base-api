3cf32050076e689623de01bc7d39bdbdd1e62ebf
medo_cyf <2238696746@qq.com>
2023-03-16 18:19:43 +0800

    更新监测点和监测组接口文档

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/MonitorDemo.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/MonitorDemo.java
@@ -17,45 +17,45 @@ public class MonitorDemo {
     /**
      * @api {POST} /MonitorPointInfo 监测点管理分页
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName MonitorPointInfo
      * @apiDescription 监测点管理分页
-     * @apiParam (请求体) {String} Name 监测点名称
-     * @apiParam (请求体) {Int} MonitorItemID 监测项目ID
+     * @apiParam (请求体) {String} [Name] 监测点名称
+     * @apiParam (请求体) {Int} [MonitorItemID] 监测项目ID
      * @apiParam (请求体) {Int} dropDownList 下拉列表-todo
      * @apiParam (请求体) {Int} pageSize 页大小
      * @apiParam (请求体) {Int} currentPage 当前页
-     * @apiSuccess (响应结果) {Object[]} list 监测点列表
-     * @apiSuccess (响应结果) {Int} list.ID 监测点ID
-     * @apiSuccess (响应结果) {Int} list.projectID 工程项目ID
-     * @apiSuccess (响应结果) {Int} list.monitorType 监测类型
-     * @apiSuccess (响应结果) {Int} list.monitorItemID 监测项目ID
-     * @apiSuccess (响应结果) {String} list.name 监测点名称
-     * @apiSuccess (响应结果) {String} list.installLocation 安装位置
-     * @apiSuccess (响应结果) {String} list.gpsLocation 地图位置
-     * @apiSuccess (响应结果) {String} list.imageLocation 底图位置
-     * @apiSuccess (响应结果) {String} list.overallViewLocation 全景位置
-     * @apiSuccess (响应结果) {String} list.spatialLocation 三维位置
-     * @apiSuccess (响应结果) {Int} list.enable 是否启用，1：启用，0：停用
-     * @apiSuccess (响应结果) {String} list.exValues 拓展字段
-     * @apiSuccess (响应结果) {String} list.displayOrder 排序字段
-     * @apiSuccess (响应结果) {Object[]} list.sensorList 监测传感器列表
-     * @apiSuccess (响应结果) {int} list.sensorList.ID 传感器ID
-     * @apiSuccess (响应结果) {int} list.sensorList.projectID 工程项目ID
-     * @apiSuccess (响应结果) {int} list.sensorList.templateID 监测类型模板ID
-     * @apiSuccess (响应结果) {String} list.sensorList.dataSourceID 数据源分布式唯一ID
-     * @apiSuccess (响应结果) {int} list.sensorList.monitorType 监测类型
-     * @apiSuccess (响应结果) {String} list.sensorList.name 传感器名称
-     * @apiSuccess (响应结果) {String} list.sensorList.alias 传感器别名
-     * @apiSuccess (响应结果) {int} list.sensorList.kind 传感器类型
-     * @apiSuccess (响应结果) {int} list.sensorList.displayOrder 显示排序字段
-     * @apiSuccess (响应结果) {int} list.sensorList.monitorPointID 关联监测点ID
-     * @apiSuccess (响应结果) {String} list.sensorList.configFieldValue 存储监测类型拓展配置值
-     * @apiSuccess (响应结果) {String} list.sensorList.exValues 拓展信息
-     * @apiSuccess (响应结果) {int} list.sensorList.status 传感器状态
-     * @apiSuccess (响应结果) {boolean} list.sensorList.warnNoData 无数据报警是否开启
-     * @apiSuccess (响应结果) {Date} list.sensorList.monitorBeginTime 监测数据开始时间
-     * @apiSuccess (响应结果) {String} list.sensorList.imagePath 传感器图片
+     * @apiSuccess (响应结果) {Object[]} monitorPointList 监测点列表
+     * @apiSuccess (响应结果) {Int} monitorPointList.ID 监测点ID
+     * @apiSuccess (响应结果) {Int} monitorPointList.projectID 工程项目ID
+     * @apiSuccess (响应结果) {Int} monitorPointList.monitorType 监测类型
+     * @apiSuccess (响应结果) {Int} monitorPointList.monitorItemID 监测项目ID
+     * @apiSuccess (响应结果) {String} monitorPointList.name 监测点名称
+     * @apiSuccess (响应结果) {String} monitorPointList.installLocation 安装位置
+     * @apiSuccess (响应结果) {String} monitorPointList.gpsLocation 地图位置
+     * @apiSuccess (响应结果) {String} monitorPointList.imageLocation 底图位置
+     * @apiSuccess (响应结果) {String} monitorPointList.overallViewLocation 全景位置
+     * @apiSuccess (响应结果) {String} monitorPointList.spatialLocation 三维位置
+     * @apiSuccess (响应结果) {Int} monitorPointList.enable 是否启用，1：启用，0：停用
+     * @apiSuccess (响应结果) {String} monitorPointList.exValues 拓展字段
+     * @apiSuccess (响应结果) {String} monitorPointList.displayOrder 排序字段
+     * @apiSuccess (响应结果) {Object[]} monitorPointList.sensorList 监测传感器列表
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.ID 传感器ID
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.projectID 工程项目ID
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.templateID 监测类型模板ID
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.dataSourceID 数据源分布式唯一ID
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.monitorType 监测类型
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.name 传感器名称
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.alias 传感器别名
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.kind 传感器类型
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.displayOrder 显示排序字段
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.monitorPointID 关联监测点ID
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.configFieldValue 存储监测类型拓展配置值
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.exValues 拓展信息
+     * @apiSuccess (响应结果) {int} monitorPointList.sensorList.status 传感器状态
+     * @apiSuccess (响应结果) {boolean} monitorPointList.sensorList.warnNoData 无数据报警是否开启
+     * @apiSuccess (响应结果) {Date} monitorPointList.sensorList.monitorBeginTime 监测数据开始时间
+     * @apiSuccess (响应结果) {String} monitorPointList.sensorList.imagePath 传感器图片
      * @apiSuccess (返回结果) {Int} totalCount 数据总量
      * @apiSuccess (返回结果) {Int} totalPage 总页数
      * @apiSampleRequest off
@@ -68,7 +68,7 @@ public class MonitorDemo {
     /**
      * @api {POST} /AddMonitorPoint 新增监测点
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName AddMonitorPoint
      * @apiDescription 新增监测点
      * @apiParam (请求体) {Int} projectID 工程项目ID
@@ -79,14 +79,13 @@ public class MonitorDemo {
      * @apiSampleRequest off
      * @apiPermission 系统权限:add
      * */
-    public Object addMonitorPoint(){
-        return null;
+    public void addMonitorPoint(){
     }
 
     /**
      * @api {POST} /DeleteMonitorPoint 删除监测点
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName DeleteMonitorPoint
      * @apiDescription 删除监测点
      * @apiParam (请求体) {Int} ID 监测点ID
@@ -94,42 +93,35 @@ public class MonitorDemo {
      * @apiSampleRequest off
      * @apiPermission 系统权限:delete
      * */
-    public Object deleteMonitorPoint(){
+    public void deleteMonitorPoint(){
         //监测点下有传感器不允许删除-todo
-        return null;
     }
 
     /**
      * @api {POST} /UpdateMonitorPoint 修改监测点
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName UpdateMonitorPoint
      * @apiDescription 修改监测点
      * @apiParam (请求体) {Int} ID 监测点ID
-     * @apiParam (请求体) {Int} projectID 工程项目ID
-     * @apiParam (请求体) {Int} monitorType 监测类型
-     * @apiParam (请求体) {Int} monitorItemID 监测项目ID
-     * @apiParam (请求体) {String} name 监测点名称
-     * @apiParam (请求体) {String} installLocation 安装位置
-     * @apiParam (请求体) {String} gpsLocation 地图位置
-     * @apiParam (请求体) {String} imageLocation 底图位置
-     * @apiParam (请求体) {String} overallViewLocation 全景位置
-     * @apiParam (请求体) {String} spatialLocation 三维位置
-     * @apiParam (请求体) {Int} enable 是否启用
-     * @apiParam (请求体) {String} exValues 拓展字段
-     * @apiParam (请求体) {String} displayOrder 排序字段
+     * @apiParam (请求体) {String} [name] 监测点名称
+     * @apiParam (请求体) {String} [installLocation] 安装位置
+     * @apiParam (请求体) {String} [gpsLocation] 地图位置
+     * @apiParam (请求体) {String} [imageLocation] 底图位置
+     * @apiParam (请求体) {String} [overallViewLocation] 全景位置
+     * @apiParam (请求体) {String} [spatialLocation] 三维位置
+     * @apiParam (请求体) {Int} [enable] 是否启用
      * @apiSuccess (返回结果) {Int} code 响应码
      * @apiSampleRequest off
      * @apiPermission 系统权限:update
      * */
-    public Object updateMonitorPoint(){
-        return null;
+    public void updateMonitorPoint(){
     }
 
     /**
      * @api {POST} /queryMonitorPoint 查询单个监测点
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName queryMonitorPoint
      * @apiDescription 查询单个监测点
      * @apiParam (请求体) {Int} ID 监测点ID
@@ -173,25 +165,24 @@ public class MonitorDemo {
     /**
      * @api {POST} /MonitorPointConfig 监测点操作-配置-todo
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName MonitorPointConfig
      * @apiDescription 监测点操作-配置
      * @apiParam (请求体) {Int} ID 监测点ID
      * @apiParam (请求体) {Object[]} sensorList 监测传感器列表
-     * @apiParam (请求体) {Object} sensorList.oneID 传感器1
-     * @apiParam (请求体) {Object} sensorList.twoID 传感器2
+     * @apiParam (请求体) {Object} [sensorList.oneID] 传感器1
+     * @apiParam (请求体) {Object} [sensorList.twoID] 传感器2
      * @apiSuccess (返回结果) {Int} code 响应码
      * @apiSampleRequest off
      * @apiPermission 系统权限:update
      * */
-    public Object monitorPointConfig(){
-        return null;
+    public void monitorPointConfig(){
     }
 
     /**
      * @api {POST} /MonitorPointData 监测点操作-数据-还没做
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测点模块
      * @apiName MonitorPointData
      * @apiDescription 监测点操作-数据
      * @apiParam (请求体) {Int} monitorID 监测点ID
@@ -204,56 +195,53 @@ public class MonitorDemo {
      * @apiSampleRequest off
      * @apiPermission 系统权限:update
      * */
-    public Object monitorPointData(){
-        return null;
+    public void monitorPointData(){
     }
 
     /**
      * @api {POST} /AddMonitorGroup 新建监测组别
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName AddMonitorGroup
      * @apiDescription 新建监测组别
      * @apiParam (请求体) {Int} projectID 工程项目ID
      * @apiParam (请求体) {String} name 监测组别名称
      * @apiParam (请求体) {Int} enable 监测组别状态
-     * @apiParam (请求体) {Object[]} list 监测项目列表
-     * @apiParam (请求体) {Int} list.monitorItemID 监测项目列表
+     * @apiParam (请求体) {Object[]} monitorItemList 监测项目列表
+     * @apiParam (请求体) {Int} monitorItemList.monitorItemID 监测项目ID
      * @apiSuccess (返回结果) {Int} code 响应码
      * @apiSampleRequest off
      * @apiPermission 系统权限:add
      * */
-    public Object addMonitorGroup(){
-        return null;
+    public void addMonitorGroup(){
     }
 
     /**
      * @api {POST} /UpdateMonitorGroup 修改监测组别
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName UpdateMonitorGroup
      * @apiDescription 修改监测组别
-     * @apiParam (请求体) {Int} ID ID
+     * @apiParam (请求体) {Int} ID 监测组别ID
      * @apiParam (请求体) {Int} projectID 工程项目ID
-     * @apiParam (请求体) {String} name 监测组别名称
-     * @apiParam (请求体) {Int} enable 监测组别状态
-     * @apiParam (请求体) {Object[]} list 监测项目列表
-     * @apiParam (请求体) {Int} list.monitorItemID 监测项目列表
+     * @apiParam (请求体) {String} [name] 监测组别名称
+     * @apiParam (请求体) {Int} [enable] 监测组别状态
      * @apiParam (请求体) {Object[]} monitorItemList 监测项目列表
-     * @apiParam (请求体) {Int} monitorItemList.ID 监测组ID
-     * @apiParam (请求体) {Int} monitorItemList.enable 监测组状态
+     * @apiParam (请求体) {Int} [monitorItemList.monitorItemID] 监测项目列表
+     * @apiParam (请求体) {Object[]} monitorSetList 监测项目列表
+     * @apiParam (请求体) {Int} monitorSetList.ID 监测组ID
+     * @apiParam (请求体) {Int} [monitorSetList.enable] 监测组状态
      * @apiSuccess (返回结果) {Int} code 响应码
      * @apiSampleRequest off
      * @apiPermission 系统权限:update
      * */
-    public Object updateMonitorGroup(){
-        return null;
+    public void updateMonitorGroup(){
     }
 
     /**
      * @api {POST} /DeleteMonitorGroup 删除监测组别
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName DeleteMonitorGroup
      * @apiDescription 删除监测组别
      * @apiParam (请求体) {Int} ID 监测组别ID
@@ -268,7 +256,7 @@ public class MonitorDemo {
     /**
      * @api {POST} /monitorGroupList 监测组别分页查询
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName monitorGroupList
      * @apiParam (请求体) {Int} name 监测组别名称
      * @apiParam (请求体) {Int} monitorItemID 监测项目ID
@@ -303,7 +291,7 @@ public class MonitorDemo {
     /**
      * @api {POST} /AddMonitorSet 新建监测组
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName AddMonitorSet
      * @apiDescription 新建监测组
      * @apiParam (请求体) {Int} ID 监测组别ID
@@ -316,20 +304,18 @@ public class MonitorDemo {
      * @apiSampleRequest off
      * @apiPermission 系统权限:add
      * */
-    public Object addMonitorSet(){
-        return null;
+    public void addMonitorSet(){
     }
 
     /**
-     * @api {POST} /UpdateMonitorSet 修改监测组别
+     * @api {POST} /UpdateMonitorSet 修改监测组
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName UpdateMonitorSet
-     * @apiDescription 修改监测组别
+     * @apiDescription 修改监测组
      * @apiParam (请求体) {Int} ID 监测组ID
-     * @apiParam (请求体) {Int} projectID 工程项目ID
-     * @apiParam (请求体) {String} name 监测组名称
-     * @apiParam (请求体) {Int} enable 监测组状态
+     * @apiParam (请求体) {String} [name] 监测组名称
+     * @apiParam (请求体) {Int} [enable] 监测组状态
      * @apiParam (请求体) {Object[]} list 监测点列表
      * @apiParam (请求体) {Int} list.ID 监测点ID
      * @apiParam (请求体) {String} list.ImageLocation 监测点底图位置
@@ -338,21 +324,19 @@ public class MonitorDemo {
      * @apiSampleRequest off
      * @apiPermission 系统权限:update
      * */
-    public Object updateMonitorSet(){
-        return null;
+    public void updateMonitorSet(){
     }
 
     /**
      * @api {POST} /QueryMonitorItemList 查询监测项目列表
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName QueryMonitorItemList
      * @apiDescription 查询监测项目列表
      * @apiParam (请求体) {String} CompanyID 公司ID
-     * @apiParam (请求体) {String} monitorProject 监测项目
-     * @apiParam (请求体) {Int} CreateType 创建类型
+     * @apiParam (请求体) {String} [name] 监测项目
      * @apiSuccess (响应结果) {Object[]} list 监测项目列表
-     * @apiSuccess (响应结果) {Int} list.ID ID
+     * @apiSuccess (响应结果) {Int} list.ID 监测项目ID
      * @apiSuccess (响应结果) {Int} list.companyID 公司ID
      * @apiSuccess (响应结果) {Int} list.monitorType 监测类型
      * @apiSuccess (响应结果) {String} list.name 监测项目名称
@@ -368,15 +352,15 @@ public class MonitorDemo {
         return null;
     }
     /**
-     * @api {POST} /QueryMonitorPointList 查询监测点列
+     * @api {POST} /QueryMonitorPointList 查询监测点列表
      * @apiVersion 1.0.0
-     * @apiGroup 监测模块
+     * @apiGroup 监测组模块
      * @apiName QueryMonitorPointList
      * @apiDescription 查询监测点列表
      * @apiParam (请求体) {String} companyID 公司ID
-     * @apiParam (请求体) {String} name 监测点名称
+     * @apiParam (请求体) {String} [name] 监测点名称
      * @apiParam (请求体) {Object[]} list 监测项目列表
-     * @apiParam (请求体) {Int} list.monitorItemID 监测项目ID
+     * @apiParam (请求体) {Int} [list.monitorItemID] 监测项目ID
      * @apiSuccess (响应结果) {Object[]} list 监测点列表
      * @apiSuccess (响应结果) {String} list.ID 监测点ID
      * @apiSuccess (响应结果) {String} list.projectID 工程项目ID
