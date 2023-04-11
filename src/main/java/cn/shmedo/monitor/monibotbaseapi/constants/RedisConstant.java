f52e7a0dd9b3be1d14ce90d1f3a3fe5e00144013
Chengfs <fengsheng.cheng@shmedo.cn>
2023-04-11 16:56:48 +0800

    redis多数据源

---
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/constants/RedisConstant.java
@@ -0,0 +1,45 @@
+package cn.shmedo.monitor.monibotbaseapi.constants;
+
+import cn.shmedo.monitor.monibotbaseapi.service.redis.RedisService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * redis 常量类
+ *
+ * @author Chengfs on 2023/3/1
+ */
+public class RedisConstant {
+
+    /**
+     * {@link StringRedisTemplate} 的beanName后缀
+     */
+    public static final String TEMPLATE_SUFFIX = "RedisTemplate";
+
+    /**
+     * {@link RedisService} 的beanName后缀
+     */
+    public static final String SERVICE_SUFFIX = "RedisService";
+
+    /**
+     * 物联网平台redisTemplate
+     */
+    public static final String IOT_REDIS_TEMPLATE = "iotRedisTemplate";
+
+    /**
+     * 监测平台redisTemplate
+     */
+    public static final String MONITOR_REDIS_TEMPLATE = "monitorRedisTemplate";
+
+    /**
+     * 监测平台redisService
+     */
+    public static final String MONITOR_REDIS_SERVICE = "monitorRedisService";
+
+    /**
+     * 物联网平台redisService
+     */
+    public static final String IOT_REDIS_SERVICE = "iotRedisService";
+}
+
+    
+    
