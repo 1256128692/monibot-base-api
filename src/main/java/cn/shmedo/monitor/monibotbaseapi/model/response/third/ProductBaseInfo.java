ec1f795caf2ebbf50fd79b1092f0a40ed70450fb
medo_kyx <youxian.kong@shmedo.cn>
2023-05-10 15:49:43 +0800

    解决分页无法获取到产品名问题

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/model/response/third/ProductBaseInfo.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/model/response/third/ProductBaseInfo.java
@@ -5,25 +5,25 @@ package cn.shmedo.monitor.monibotbaseapi.model.response.third;
  * @date: 2023-05-06 14:58
  */
 public class ProductBaseInfo {
-    private Integer ID;
+    private Integer productID;
     private String productName;
     private String productToken;
 
     public ProductBaseInfo() {
     }
 
-    public ProductBaseInfo(Integer ID, String productName, String productToken) {
-        this.ID = ID;
+    public ProductBaseInfo(Integer productID, String productName, String productToken) {
+        this.productID = productID;
         this.productName = productName;
         this.productToken = productToken;
     }
 
-    public Integer getID() {
-        return ID;
+    public Integer getProductID() {
+        return productID;
     }
 
-    public void setID(Integer ID) {
-        this.ID = ID;
+    public void setProductID(Integer productID) {
+        this.productID = productID;
     }
 
     public String getProductName() {
