11ebcd397117769ff929a58fd333024dbe8119d6
gaoxu <gaoxao004@gmail.com>
2023-06-07 14:32:23 +0800

    fix:QueryMonitorTypePage接口添加projectID参数

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/MonitorTypeController.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/MonitorTypeController.java
@@ -205,6 +205,7 @@ public class MonitorTypeController {
      * @apiDescription 查询监测类型分页
      * @apiParam (请求参数) {Int} companyID 公司ID
      * @apiParam (请求参数) {Int} [monitorType] 监测类型
+     * @apiParam (请求参数) {Int} [projectID]  项目ID, 该项存在时候,返回工程项目对应的监测项目的监测类型
      * @apiParam (请求参数) {Int} [createType] 定义类型，01预定义自定义, 注意排序，自定义在前
      * @apiParam (请求参数) {String} [queryCode] 检索关键字，可匹配  属性名称， 属性标识
      * @apiParam (请求参数) {Boolean} [allFiled] 全属性，否则fieldClass只展示12
