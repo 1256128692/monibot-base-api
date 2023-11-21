24314b04d8311b3b9723a235b19d0862b2ce665a
Chengfs <fengsheng.cheng@shmedo.cn>
2023-11-21 18:05:27 +0800

    docs: 闸泵控制模块文档

---
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/controller/SluiceController.java
@@ -0,0 +1,177 @@
+package cn.shmedo.monitor.monibotbaseapi.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 闸泵控制
+ *
+ * @author Chengfs on 2023/11/16
+ */
+public class SluiceController {
+
+    /**
+     * @api {POST} /QuerySluicePage 分页查询水闸列表
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName QuerySluicePage
+     * @apiDescription 分页查询水闸列表
+     * @apiParam (请求参数) {Int} companyID 公司ID
+     * @apiParam (请求参数) {String} [keyword] 水闸名称/渠道名称 模糊查询
+     * @apiParam (请求参数) {String} [sluiceType] 水闸类型 (进水闸、退水闸、船闸、节制闸、挡潮闸、其他)
+     * @apiParam (请求参数) {Int} [controlType] 控制类型 1远程控制 2手动控制 3现地控制
+     * @apiParam (请求参数) {String} [manageUnit] 管理单位
+     * @apiParam (请求参数) {Int} currentPage 当前页(>=1)
+     * @apiParam (请求参数) {Int} pageSize 页大小(1-100)
+     * @apiSuccess (返回结果) {Int} totalCount 总条数
+     * @apiSuccess (返回结果) {Int} totalPage 总页数
+     * @apiSuccess (返回结果) {Object[]} currentPageData 当前页数据
+     * @apiSuccess (返回结果) {Int} currentPageData.projectID 项目id
+     * @apiSuccess (返回结果) {Int} currentPageData.monitorID 监测点id
+     * @apiSuccess (返回结果) {String} currentPageData.projectName 水闸名称
+     * @apiSuccess (返回结果) {String} currentPageData.canal 所属渠道
+     * @apiSuccess (返回结果) {Int} currentPageData.sluiceType 水闸类型 (进水闸、退水闸、船闸、节制闸、挡潮闸、其他)
+     * @apiSuccess (返回结果) {String} currentPageData.manageUnit 管理单位
+     * @apiSuccess (返回结果) {Int} currentPageData.sluiceHoleNum 闸孔数量
+     * @apiSuccess (返回结果) {Int} currentPageData.openStatus 启闭状态 0.关闭 1.开启 (任一闸门开启该水闸即为开启)
+     * @apiSuccess (返回结果) {Object[]} currentPageData.gates 闸门信息
+     * @apiSuccess (返回结果) {Int} currentPageData.gates.id 闸门id (唯一, 对应监测传感器id)
+     * @apiSuccess (返回结果) {Int} currentPageData.gates.controlType 控制类型 1远程控制 2手动控制 3现地控制
+     * @apiSuccess (返回结果) {Int} currentPageData.gates.openStatus 启闭状态 0.关闭 1.开启
+     * @apiSuccess (返回结果) {Int} currentPageData.gates.alias 闸门名称(传感器别名)
+     * @apiSuccess (返回结果) {Double} currentPageData.frontWaterLevel 闸前水位(m)
+     * @apiSuccess (返回结果) {Double} currentPageData.backWaterLevel 闸后水位(m)
+     * @apiSuccess (返回结果) {Double} currentPageData.flowRate 瞬时流量(m³/s)
+     * @apiSuccess (返回结果) {Int} currentPageData.collectSensorID 采集传感器id (用于查询采集数据)
+     * @apiSuccess (返回结果) {Int} currentPageData.videoMonitorGroupID 视频监测组id (用于查询视频监控)
+     * @apiSuccess (返回结果) {DateTime} currentPageData.lastCollectTime 最后采集时间(yyyy-MM-dd HH:mm:ss)
+     * @apiSampleRequest off
+     * @apiPermission 系统权限 mdmbase:
+     */
+    @PostMapping(value = "/QuerySluicePage", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object sluicePage(@Validated @RequestBody Void request) {
+        return null;
+    }
+
+    /**
+     * @api {POST} /SluiceControl 水闸远程控制
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName SluiceControl
+     * @apiDescription 闸门远程控制
+     * @apiParam (请求参数) {Int} projectID 项目(水闸) id
+     * @apiParam (请求参数) {Int} [gateID] 闸门id (为空时控制所有闸门, 仅允许 5开启/6关闭)
+     * @apiParam (请求参数) {Int} actionKind 操作种类 (1停止 2上升 3下降 4自动控制 5开启 6关闭)
+     * @apiParam (请求参数) {Int} [actionType] 操作类型 (仅actionKind=4有效: 1恒定水位 2恒定流量 3恒定闸位 4时长控制 5时段控制 6总量控制)
+     * @apiParam (请求参数) {Object} [target] 控制目标 (仅actionKind=4有效, 不同actionType下具有不同结构)
+     * @apiParam (请求参数) {Double} [target.waterLevel] 目标水位 (1恒定水位)
+     * @apiParam (请求参数) {Double} [target.flowRate] 目标流量 (2恒定流量)
+     * @apiParam (请求参数) {Double} [target.gateDegree] 目标开度 (3恒定闸位)
+     * @apiParam (请求参数) {DateTime} [target.openTime] 开闸时间 (4时长控制)
+     * @apiParam (请求参数) {Long} [target.duration] 运行时长(分钟) (4时长控制)
+     * @apiParam (请求参数) {DateTime} [target.beginTime] 开始时间 (5时段控制)
+     * @apiParam (请求参数) {DateTime} [target.endTime] 结束时间 (5时段控制)
+     * @apiParam (请求参数) {Double} [target.totalFlow] 目标累计流量 (6总量控制)
+     * @apiSampleRequest off
+     * @apiPermission 项目权限 mdmbase:
+     */
+    @PostMapping(value = "/SluiceControl", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object sluiceControl(@Validated @RequestBody Void request) {
+        return null;
+    }
+
+    /**
+     * @api {POST} /QuerySluiceDetail 水闸详情
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName QuerySluiceDetail
+     * @apiDescription 闸门详情
+     * @apiParam (请求参数) {Int} projectID 项目(水闸) id
+     * @apiSuccess (返回结果) {Object[]} gates 闸门列表
+     * @apiSuccess (返回结果) {Int} gates.id 闸门id (唯一, 对应监测传感器id)
+     * @apiSuccess (返回结果) {String} gates.alias 闸门名称 (监测传感器别名)
+     * @apiSuccess (返回结果) {Int} gates.openStatus 启闭状态 0.关闭 1.开启
+     * @apiSuccess (返回结果) {Double} gates.openDegree 闸门开度(m)
+     * @apiSuccess (返回结果) {Int} gates.powerStatus 电源状态 0.正常 1.异常
+     * @apiSuccess (返回结果) {Double} gates.powerCurrent 电源电流 (A)
+     * @apiSuccess (返回结果) {Double} gates.powerVoltage 电源电压 (V)
+     * @apiSuccess (返回结果) {Int} gates.controlType 控制类型 1远程控制 2手动控制 3现地控制
+     * @apiSuccess (返回结果) {Double} gates.maxOpenDegree 闸门最大开度
+     * @apiSuccess (返回结果) {Double} maxFlowRate 最大过闸流量
+     * @apiSuccess (返回结果) {Double} maxBackWaterLevel 最大闸后水位
+     * @apiSuccess (返回结果) {Double} minBackWaterLevel 最大闸后水位
+     * @apiSuccess (返回结果) {Double} frontWaterLevel 闸前水位(m)
+     * @apiSuccess (返回结果) {Double} backWaterLevel 闸后水位(m)
+     * @apiSuccess (返回结果) {Double} flowRate 瞬时流量(m³/s)
+     * @apiSuccess (返回结果) {Double} flowTotal 累计流量
+     * @apiSampleRequest off
+     * @apiPermission 项目权限 mdmbase:
+     */
+    @PostMapping(value = "/QuerySluiceDetail", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object sluiceDetail(@Validated @RequestBody Void request) {
+        return null;
+    }
+
+    /**
+     * @api {POST} /ListSluice 水闸列表
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName ListSluice
+     * @apiDescription 水闸列表 (下拉)
+     * @apiParam (请求参数) {Int} companyID 公司ID
+     * @apiParam (请求参数) {Int} [projectID] 项目(水闸) id
+     * @apiParam (请求参数) {Int} [controlType] 控制类型 (1远程控制 2手动控制 3现地控制)
+     * @apiSuccess (返回结果) {Object[]} data 水闸列表
+     * @apiSuccess (返回结果) {Int} data.projectID 项目(水闸) id
+     * @apiSuccess (返回结果) {String} data.projectName 项目(水闸) 名称
+     * @apiSuccess (返回结果) {String} data.canal 所属渠道
+     * @apiSuccess (返回结果) {Int} data.sluiceType 水闸类型 (进水闸、退水闸、船闸、节制闸、挡潮闸、其他)
+     * @apiSuccess (返回结果) {String} data.manageUnit 管理单位
+     * @apiSuccess (返回结果) {Int} data.sluiceHoleNum 闸孔数量
+     * @apiSampleRequest off
+     * @apiPermission 项目权限 mdmbase:
+     */
+    @PostMapping(value = "/ListSluiceSimple", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object listSluiceSimple(@Validated @RequestBody Void request) {
+        return null;
+    }
+
+    /**
+     * @api {POST} /ListSluiceManageUnit 水闸管理单位列表
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName ListSluiceManageUnit
+     * @apiDescription 水闸管理单位列表 (下拉)
+     * @apiParam (请求参数) {Int} companyID 公司ID
+     * @apiParam (请求参数) {Int} [projectID] 项目(水闸) id
+     * @apiSuccess (返回结果) {String[]} data 管理单位列表
+     * @apiSampleRequest off
+     * @apiPermission 项目权限 mdmbase:
+     */
+    @PostMapping(value = "/ListSluiceManageUnit", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object listSluiceManageUnit(@Validated @RequestBody Void request) {
+        return null;
+    }
+
+    /**
+     * @api {POST} /ListSluiceGate 水闸闸门列表
+     * @apiVersion 1.0.0
+     * @apiGroup 闸泵控制模块
+     * @apiName ListSluiceGate
+     * @apiDescription 水闸闸门列表 (下拉)
+     * @apiParam (请求参数) {Int} companyID 公司ID
+     * @apiParam (请求参数) {Int} [projectID] 项目(水闸) id
+     * @apiParam (请求参数) {Int} [gateID] 传感器(闸门) id
+     * @apiSuccess (返回结果) {Object[]} data 闸门列表
+     * @apiSuccess (返回结果) {Int} data.id 监测传感器(闸门) id
+     * @apiSuccess (返回结果) {String} data.alias 闸门名称 (监测传感器别名)
+     * @apiSampleRequest off
+     * @apiPermission 项目权限 mdmbase:
+     */
+    @PostMapping(value = "/ListSluiceGate", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Object listSluiceGate(@Validated @RequestBody Void request) {
+        return null;
+    }
+}
