a21ec633524a2639031ca10eaae28dc5152b6e5e
gaoxu <gaoxao004@gmail.com>
2023-12-04 17:49:38 +0800

    chore： 删除“跨域全部”的http头的填充

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/interceptor/CustomCorsFilter.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/interceptor/CustomCorsFilter.java
@@ -14,7 +14,7 @@ public class CustomCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
+//        res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PUT");
         res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type,access_token,access_type,content-type,app_key,app_secret");
