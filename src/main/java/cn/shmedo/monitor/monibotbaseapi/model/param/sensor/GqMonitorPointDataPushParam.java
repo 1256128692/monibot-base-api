5f8294e0faf840f00af2dd373d89dcbc4302088d
medo_rjx <medo123456>
2023-12-18 10:26:06 +0800

    fix:接口(influxdb原始数据补录接口新增数据类型校验)

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/model/param/sensor/GqMonitorPointDataPushParam.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/model/param/sensor/GqMonitorPointDataPushParam.java
@@ -2,6 +2,8 @@ package cn.shmedo.monitor.monibotbaseapi.model.param.sensor;
 
 import cn.shmedo.iot.entity.api.*;
 import cn.shmedo.iot.entity.api.permission.ResourcePermissionProvider;
+import cn.shmedo.monitor.monibotbaseapi.util.base.CollectionUtil;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -15,11 +17,25 @@ public class GqMonitorPointDataPushParam implements ParameterValidator, Resource
     @NotNull(message = "监测类型不能为空")
     private Integer monitorType;
 
+    @NotEmpty
     private List<SensorInfluxdbData> dataList;
 
 
     @Override
     public ResultWrapper<?> validate() {
+
+        for (int i = 0; i < dataList.size(); i++) {
+            if (CollectionUtil.isNullOrEmpty(dataList.get(i).getSensorDataList())) {
+                return ResultWrapper.withCode(ResultCode.INVALID_PARAMETER, "补录数据不能为空");
+            }
+            for (int j = 0; j < dataList.get(i).getSensorDataList().size(); j++) {
+                if (!(dataList.get(i).getSensorDataList().get(j).getValue() instanceof Integer
+                        || dataList.get(i).getSensorDataList().get(j).getValue() instanceof Long
+                        || dataList.get(i).getSensorDataList().get(j).getValue() instanceof Double)) {
+                    return ResultWrapper.withCode(ResultCode.INVALID_PARAMETER, "补录数据类型必须为long或者double类型");
+                }
+            }
+        }
         return null;
     }
 
