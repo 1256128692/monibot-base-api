2dbf44243520ef8bec3ff3d35c3e073932d9d6e4
medo_kyx <youxian.kong@shmedo.cn>
2024-01-16 16:52:11 +0800

    feat:比较方式枚举类实现

---
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/model/enums/CompareMode.java
@@ -0,0 +1,37 @@
+package cn.shmedo.monitor.monibotbaseapi.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * 比较方式
+ *
+ * @author youxian.kong@shmedo.cn
+ * @date 2024-01-16 16:40
+ */
+@Getter
+@RequiredArgsConstructor
+public enum CompareMode {
+    IN(1, 2, "在区间内"),
+    OUT(2, 2, "偏离区间"),
+    GT(3, 1, "大于"),
+    GTE(4, 1, "大于等于"),
+    LT(5, 1, "小于"),
+    LTE(6, 1, "小于等于");
+
+    private final Integer code;
+    /**
+     * 报警等级阈值配置json中,含有元素的个数<br>
+     * 区间相关的比较方式,会有上限、下限两个值; 其他情况下暂时只有一个值
+     */
+    private final Integer fieldCount;
+    private final String desc;
+
+    public static CompareMode fromCode(final Integer code) {
+        return Optional.ofNullable(code).flatMap(u -> Arrays.stream(values()).filter(w -> w.getCode().equals(u)).findAny())
+                .orElseThrow(() -> new IllegalArgumentException("Illegal CompareMode code,code: " + code));
+    }
+}
