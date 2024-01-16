049d5d8ffe26d8a56dc61c86f6d8115dae157f40
gaoxu <gaoxao004@gmail.com>
2024-01-16 14:36:33 +0800

    fix： QueryProjectType接口支持MDNET3.0服务

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/config/AuthServiceIDAndProjectTypeRelation.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/config/AuthServiceIDAndProjectTypeRelation.java
@@ -12,19 +12,27 @@ import java.util.Map;
  **/
 public class AuthServiceIDAndProjectTypeRelation {
     private static Map<Integer, List<String>> relation;
-
+    private static List<Integer> allTypeServiceList;
     static {
         relation = Map.of(
+                // 灌区
                 15, List.of("水库", "堤防", "河道", "灌区", "流域"),
+                // 流域
                 16, List.of("灌区", "水库", "河道", "渠首", "机井", "渠系", "水闸", "泵站", "田间"),
+                // 水库
                 17, List.of("水库")
         );
+        // MDNET3.0
+        allTypeServiceList = List.of(20);
     }
 
     public static boolean isLegalServiceID(Integer serviceID) {
         return relation.containsKey(serviceID);
     }
 
+    public static boolean isAllTypeService(Integer serviceID) {
+        return allTypeServiceList.contains(serviceID);
+    }
     public static boolean isLegalProjectType(Integer serviceID, String projectType) {
         return relation.get(serviceID).contains(projectType);
     }
