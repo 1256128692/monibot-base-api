d18dd19a55a54fc3c25b46b281a7e280410c4626
liuwei <wei.liu@shmedo.cn>
2024-02-04 14:05:22 +0800

    chore:修改了跨域配置

--- src/main/java/cn/shmedo/monitor/monibotbaseapi/interceptor/CustomCorsFilter.java
+++ src/main/java/cn/shmedo/monitor/monibotbaseapi/interceptor/CustomCorsFilter.java
@@ -14,10 +14,12 @@ public class CustomCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-//        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PUT");
         res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type,access_token,access_type,content-type,app_key,app_secret");
+                res.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type,access_token," +
+                "access_type,access_service,content-type,app_key,app_secret,x-md-currentuser,x-md-request-code,x-md-request-id," +
+                "Referer,x-md-set-real-ip,X-Forwarded-For,x-md-rnd-value");
         chain.doFilter(request, response);
     }
 }
